4f4c8d0fc1b91a07ecbaa81f7d4eedfb297f81a1
Patrick Coffey <pcoffey@fahrenheitmarketing.com>
2012-10-17 10:09:26 -0500

    Adding inition commit of subtheme files

--- README.txt
+++ README.txt
@@ -1,1 +1,4 @@
-Feel free to use/edit this theme. 
+#Base Building Blocks Admin
+###Twitter Bootstrap administration theme for Drupal 7
+
+Base Building Blocks Admin is a subtheme of Base Building Blocks meant to allow you to have a customized, intuitive, Twitter Bootstrap based administration theme. It loads it's own stylesheet and has it's own page template so you can customize the look and feel of you administration theme seperate from your main Base Building Blocks theme.
--- templates/page.tpl.php
+++ templates/page.tpl.php
@@ -3,13 +3,12 @@
   <?php include 'includes/page-header.php'; ?>
   <?php include 'includes/page-menu-main.php'; ?>
   <section class="row-fluid" id="main" role="document">
-    <article class="span9" id="main-content" role="article">
+    <article class="span12" id="main-content" role="article">
       <?php include 'includes/page-tabs-messages.php'; ?>
       <?php include 'includes/page-title.php'; ?>
       <?php include 'includes/page-action-links.php'; ?>
       <?php print render($page['content']); ?>
     </article>
-    <?php include 'includes/page-sidebar-right.php'; ?>
   </section>
   <?php include 'includes/page-footer.php'; ?>
 </section>
